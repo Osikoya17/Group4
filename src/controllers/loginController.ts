9cb5750bce1202dd1dfbd69fca3aff216136d6e6
Osikoya17 <osikoyaolaoluwa2021@gmail.com>
2022-09-29 12:05:12 +0100

    fixed login

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -5,16 +5,16 @@ import { Types } from "mongoose"
 import * as jwt  from "jsonwebtoken"
 import { JWT_SECRET_KEY } from "../config/env"
 export const Login = async(req:Request,res:Response) =>{
-   
+    const {email,password} = req.body
     const maxAge = 30*60*60
     const createToken  = (id:Types.ObjectId) =>{
     return jwt.sign({id},JWT_SECRET_KEY!,{
         expiresIn:maxAge
     })}
     try {
-        const user = await User.findOne(req.body?.email)
+        const user = await User.findOne({email})
             if (user){
-                const auth = await bcrypt.compare(req.body?.password,user.password)
+                const auth = await bcrypt.compare(password,user.password)
                 if(auth){
                     const token = createToken(user._id)
                     res.status(200).json({user:user,token:token,message:"User login successful"})
