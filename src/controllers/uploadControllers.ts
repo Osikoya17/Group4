5751dff0b9d5769e8c120889c1b85d8b4ea25ac0
Osikoya17 <osikoyaolaoluwa2021@gmail.com>
2022-10-17 17:32:40 +0100

    uplaod

--- src/controllers/uploadControllers.ts
+++ src/controllers/uploadControllers.ts
@@ -7,13 +7,14 @@ import path from "path"
 import { User } from "../model/Users"
 export const Upload = async(req:Request,res:Response) =>{
     // let session = null
-    const{title,story,category} = req.body
+    const{title,story,category,image} = req.body
     try {
-        const blog = new Blog({
-            title,
-            story,
-            category
-        }).save()
+        // const blog = new Blog({
+        //     title,
+        //     story,
+        //     category,
+        //     image
+        // }).save()
         // session = await startSession()
         // session.startTransaction()
         // await User.findOneAndUpdate(
@@ -29,16 +30,16 @@ export const Upload = async(req:Request,res:Response) =>{
         // )
 
         // await session.commitTransaction()
-        // const blog = await Blog.create({
-        //     title:req.body.title,
-        //     story:req.body.story,
-        //     author:req.body.author,
-        //     category:req.body.category,
-        //     image:{
-        //         data:fs.readFileSync(path.join(__dirname,"..","public/"+ req.file?.filename)),
-        //         contentType:"image/jpg"
-        //     }
-        // })
+        const blog = await Blog.create({
+            title:req.body.title,
+            story:req.body.story,
+            author:req.body.author,
+            category:req.body.category,
+            image:{
+                data:fs.readFileSync(path.join(__dirname,"..","public/"+ req.file?.filename)),
+                contentType:"image/jpg"
+            }
+        })
         res.status(200).json({message:"Uploaded Successfully",blog})
     } catch (error) {
         // await session?.abortTransaction()
